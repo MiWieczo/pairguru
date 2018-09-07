aac6a903b93b6a25648f6937f562276719adcfec
Superek1993 <dzikimiki93@gmail.com>
2018-09-08 01:52:43 +0200

    add ability to destroy comments, with restriction to the author

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,13 @@ class CommentsController < ApplicationController
 
   end
 
-  def show
-
-  end
-
   def destroy
-
+    @comment = Comment.find(params[:id])
+    if @comment.author == current_user
+      @comment.destroy
+      redirect_to movie_path(@comment.movie), notice: "Comment was successfully deleted"
+    else
+      redirect_to movie_path(@comment.movie), alert: "You cannot perform this action"
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_193315) do
+ActiveRecord::Schema.define(version: 2018_09_07_234403) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text", null: false
     t.integer "movie_id", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["movie_id"], name: "index_comments_on_movie_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "genres", force: :cascade do |t|
