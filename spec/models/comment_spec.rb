9c488b2ae789902ea5f45fac8ba23ca91dbbe530
Superek1993 <dzikimiki93@gmail.com>
2018-09-13 00:15:39 +0200

    specify specs for comment model validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,27 +1,34 @@
 require 'rails_helper'
 
 describe Comment, type: :model do
-  subject { Comment.new }
+  let(:author) { User.create name: "Miko", email: "miko@gmail.com", encrypted_password: "123456" }
+  let(:movie) { Movie.create title: "Star Wars", description: "jedi knights etc." }
+  subject { Comment.new text: "super film", movie: movie, author: author}
 
   describe "Validations" do
-    it "is valid" do
 
+    it "is valid with valid attirbutes" do
+      expect(subject).to be_valid
     end
 
-    it "has no text" do
-
+    it "is invalid without text" do
+      subject.text = nil
+      expect(subject).not_to be_valid
     end
 
-    it "does not have an author" do
-
+    it "is invalid without an author" do
+      subject.author = nil
+      expect(subject).not_to be_valid
     end
 
-    it "does not belong to a movie" do
-
+    it "is invalid without a movie" do
+      subject.movie = nil
+      expect(subject).not_to be_valid
     end
 
-    it "does not have a creation date" do
-
+    it "is invalid without creation date" do
+      subject.created_at = nil
+      expect(subject).not_to be_valid
     end
   end
 
