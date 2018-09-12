0cbc90cc2913b2c750276b891d39a9253b4a3f30
Superek1993 <dzikimiki93@gmail.com>
2018-09-12 21:51:42 +0200

    skeleton for comment model spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 describe Comment, type: :model do
+  subject { Comment.new }
+
+  describe "Validations" do
+    it "is valid" do
+
+    end
+
+    it "has no text" do
+
+    end
+
+    it "does not have an author" do
+
+    end
+
+    it "does not belong to a movie" do
+
+    end
+
+    it "does not have a creation date" do
+
+    end
+  end
+
+  describe "Associations" do
+
+  end
 
 end
