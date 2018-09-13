5039127c55d6e072fef80a86fe37ff91784b03d7
dzikimiki93 <dzikimiki93@gmail.com>
2018-09-13 08:54:34 -0400

    fix user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,6 +3,5 @@ FactoryBot.define do
     name { Faker::FunnyName.name }
     email { Faker::Internet.email }
     password { Faker::Lorem.characters(8) }
-    encrypted_password { Faker::Lorem.characters(8) }
   end
 end
