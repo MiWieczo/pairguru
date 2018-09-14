ecb782f648964887e804ad2af66f38ac00f68021
Superek1993 <dzikimiki93@gmail.com>
2018-09-14 09:42:38 +0200

    finish users requests spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,5 +1,21 @@
 require "rails_helper"
 
 describe "Users requests", type: :request do
-  
+  context "top users list" do
+    before(:each) do
+      visit top_users_path
+    end
+
+    let!(:ten_users) { create_list(:user, 10, :with_comments)}
+
+    it "displays right title" do
+      expect(page).to have_selector("h1", text: "Top ten users of the week")
+    end
+
+    it "displays table of ten users" do
+      puts page.has_selector?(".top-users-row")
+      puts page.all(".top-users-row").count
+      # expect(page).to have_selector(".top-users-row", count: 10)
+    end
+  end
 end
