7c86807eb362899fdf8699766eb80c8d8ed054a6
dzikimiki93 <dzikimiki93@gmail.com>
2018-09-14 05:39:00 -0400

    finish user request spec, final

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,20 +2,23 @@ require "rails_helper"
 
 describe "Users requests", type: :request do
   context "top users list" do
-    before(:each) do
-      visit top_users_path
-    end
-
-    let!(:ten_users) { create_list(:user, 10, :with_comments)}
+    users_sample = rand(5..20)
+    let!(:users_list) { create_list(:user, users_sample, :with_comments)}
 
     it "displays right title" do
+      visit top_users_path
       expect(page).to have_selector("h1", text: "Top ten users of the week")
     end
 
     it "displays table of ten users" do
-      puts page.has_selector?(".top-users-row")
-      puts page.all(".top-users-row").count
-      # expect(page).to have_selector(".top-users-row", count: 10)
+      get '/top_users'
+      expect(response.code).to eq("200")
+      expect(response).to render_template(:display_top_users)
+      if users_sample < 10
+        expect(response.body).to have_selector(".top-users-row", count: users_sample)
+      else
+        expect(response.body).to have_selector(".top-users-row", count: 10)
+      end
     end
   end
 end
